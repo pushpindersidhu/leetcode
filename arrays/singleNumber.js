3b3df159d7829efbc1044a6c95d7bb00b84524d6
sidhu <pushpindersidhu1984@gmail.com>
2022-09-17 16:18:11 -0700

    Single Number

--- arrays/singleNumber.js
+++ arrays/singleNumber.js
@@ -3,7 +3,15 @@
  * @return {number}
  */
 var singleNumber = function (nums) {
-    for (const num in nums) {
-        if (num in nums) 
+  for (let i = 0; i < nums.length; i++) {
+    let found = false;
+    for (let j = 0; j < nums.length; j++) {
+      if (i === j) continue;
+      if (nums[i] === nums[j]) {
+        found = true;
+        break;
+      }
     }
+    if (!found) return nums[i];
+  }
 };
