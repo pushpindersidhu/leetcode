cca529bee1a16e2eb2d68aed275de8ac5040c47e
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-09-29 19:58:23 -0700

    Binary Search

---
+++ python/binary_search/binary_search.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self.binary_search(nums, target, 0, len(nums) - 1)
+    
+    def binary_search(self, nums: List[int], target: int, left: int, right: int) -> int:
+        if left > right:
+            return -1
+        
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        
+        if nums[mid] > target:
+            return self.binary_search(nums, target, left, mid - 1)
+        else:
+            return self.binary_search(nums, target, mid + 1, right)
+        
