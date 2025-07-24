d18615dba743447910a2b418fa5af50c1144ac54
sidhu <pushpindersidhu1984@gmail.com>
2025-07-24 01:49:57 -0700

    Product of array except self

---
+++ python/arrays_and_hashing/product_of_array_except_self.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = [1] * len(nums)
+        for i in range(1, len(nums)):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+
+        postfix = [1] * len(nums)
+        for i in range(len(nums) - 2, -1, -1):
+            postfix[i] = postfix[i + 1] * nums[i + 1]
+
+        product = [1] * len(nums)
+        for i in range(len(nums)):
+            product[i] = prefix[i] * postfix[i]
+
+        return product
