3aba8dc3b76e6b5eca01cc3cb8d4824b01ac0b33
sidhu <pushpindersidhu1984@gmail.com>
2025-07-24 00:47:27 -0700

    Group Anagrams

---
+++ python/arrays_and_hashing/group_anagrams.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        out = defaultdict(list)
+
+        for s in strs:
+            out["".join(sorted(s))].append(s)
+
+        return list(out.values())
